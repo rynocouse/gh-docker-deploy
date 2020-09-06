8eb60f091620572e29232d2591d0e80b30f86694
Ryan Couse <ryan@madeinhaus.com>
2020-09-06 00:56:11 -0700

    Setup remove and prune

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const ssh_public_key = core.getInput('ssh_public_key', { required: true });
 const stack_file_name = core.getInput('stack_file_name', { required: true });
 const project_name = core.getInput('project_name', { required: true });
 
+const action = core.getInput('action', { required: true });
+
 const awsRegion = core.getInput('region') || process.env.AWS_DEFAULT_REGION || 'us-east-1';
 
 function run(cmd, options = {}) {
@@ -32,37 +34,29 @@ run(`$(aws ecr get-login --no-include-email --region ${awsRegion})`);
 const accountData = run(`aws sts get-caller-identity --output json`);
 const awsAccountId = JSON.parse(accountData).Account;
 
-// console.log(
-//     `Pushing local image ${localImage} to ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`
-// );
-// run(`docker tag ${localImage} ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
-// run(`docker push ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${image}`);
-
 run(`echo "Registering SSH keys..."`);
 
 // # register the private key with the agent.
 run(`mkdir -p "$HOME/.ssh"`);
-
 run(`printf '%s\n' "${ssh_private_key}" > "$HOME/.ssh/id_rsa"`);
-
 run(`chmod 600 "$HOME/.ssh/id_rsa"`);
 run(`eval $(ssh-agent) && ssh-add "$HOME/.ssh/id_rsa"`);
 
-// run(`echo "Add known hosts"`);
-// run(`sudo chmod 600 "/etc/ssh/ssh_known_hosts"`);
+if (action === 'deploy') {
+    run(
+        `DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose --log-level debug --host ssh://${remote_docker_host} -f ${stack_file_name} pull --ignore-pull-failures`
+    );
+    run(
+        `DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose -p ${project_name} --log-level debug --host ssh://${remote_docker_host} -f ${stack_file_name} up -d`
+    );
+}
 
-// run(`sudo printf '%s %s\n' "${remote_docker_host}" "${ssh_public_key}" > /etc/ssh/ssh_known_hosts`);
-
-run(
-    `DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose --log-level debug --host ssh://${remote_docker_host} -f ${stack_file_name} pull --ignore-pull-failures`
-);
-
-run(
-    `DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose -p ${project_name} --log-level debug --host ssh://${remote_docker_host} -f ${stack_file_name} up -d`
-);
-
-// echo "Connecting to $remote_docker_host... Command: docker-compose --log-level debug --host ssh://$remote_docker_host -f $stack_file_name pull --ignore-pull-failures"
-// DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose --log-level debug --host ssh://$remote_docker_host -f $stack_file_name pull --ignore-pull-failures 2>&1
-
-// echo "Connecting to $remote_docker_host... Command: docker-compose -p ${project_name} --log-level debug --host ssh://$remote_docker_host -f $stack_file_name up -d"
-// DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose -p ${project_name} --log-level debug --host ssh://$remote_docker_host -f $stack_file_name up -d 2>&1
+if (action === 'remove') {
+    run(
+        `DOCKER_HOST="tcp://127.0.0.1:2375" docker-compose -p ${project_name} --log-level debug --host ssh://${remote_docker_host} -f ${stack_file_name} stop`
+    );
+    // Kill all stopped containers
+    run(
+        `DOCKER_HOST="tcp://127.0.0.1:2375" docker --host ssh://${remote_docker_host} system prune -a --volumes -f`
+    );
+}
